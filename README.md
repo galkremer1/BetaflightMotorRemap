f7d06efda452f13487866bd47e0acb926b18a781
galkremer1 <galkremer1@walla.co.il>
2020-01-20 21:55:35 +0200

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-## Live Demo:
-https://galkremer1.github.io/BetaflightMotorRemap/
-
-- Add option for more than 4 motors (later)
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./style/index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import Application from "./App";
-
-ReactDOM.render(<Application />, document.getElementById("root"));
