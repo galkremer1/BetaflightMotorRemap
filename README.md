b2d7bc1feb6032024f31601d052a58a5449610ba
galkremer1 <galkremer1@walla.co.il>
2020-11-21 20:41:25 +0200

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-## Live Demo:
-
-http://kremerfpv.info
-
-Note:
-Create CNAME file at master branch manually each deploy.
---
+++ precache-manifest.aed440b1014b73fbf4c502444d95baf4.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "6961c69c2378d3d84b8d",
+    "url": "/static/css/main.fe0550d3.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "e1abd3fd61f58b45388bbaa4e95e3beb",
+    "url": "/static/media/patreon.e1abd3fd.png"
+  },
+  {
+    "revision": "b53504f9990fb45c1f53",
+    "url": "/static/js/2.61571c4b.chunk.js"
+  },
+  {
+    "revision": "b9cf8ce5743db4f9099c4d8a59ea541a",
+    "url": "/static/media/kremerfpvlogo.b9cf8ce5.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "6961c69c2378d3d84b8d",
+    "url": "/static/js/main.de8dea80.chunk.js"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "2511b408ead13863b9266f320ac0c5cc",
+    "url": "/static/media/under-construction.2511b408.png"
+  },
+  {
+    "revision": "cfcbc1972d868b5cd2f80f9758344cea",
+    "url": "/static/media/65a.cfcbc197.png"
+  },
+  {
+    "revision": "45fb33aee6c0c7ad80adc6d39317c6f6",
+    "url": "/static/media/65aFlipped.45fb33ae.png"
+  },
+  {
+    "revision": "c73df4b745d958f66b196029a698a65a",
+    "url": "/static/media/youtube.c73df4b7.svg"
+  },
+  {
+    "revision": "a09dab886d05c6bc5f14d29497f679fb",
+    "url": "/static/media/PayPal.a09dab88.png"
+  },
+  {
+    "revision": "bf5a084ed1b9338cb600d7c943d75515",
+    "url": "/static/media/beer.bf5a084e.png"
+  },
+  {
+    "revision": "0108f6c86e5228699381fe1884733230",
+    "url": "/static/media/link.0108f6c8.png"
+  },
+  {
+    "revision": "ae5701fb21c274c27d3fda55bba8d196",
+    "url": "/static/media/helination.ae5701fb.png"
+  },
+  {
+    "revision": "dd3b60321ad6f19388d7c74236074ce9",
+    "url": "/static/media/rotorriot.dd3b6032.png"
+  },
+  {
+    "revision": "7c49f1c66f1401a1dbb211230cfed590",
+    "url": "/static/media/banggood.7c49f1c6.png"
+  },
+  {
+    "revision": "d40e585dc3e2a58880fb20094c624133",
+    "url": "/static/media/gearbest.d40e585d.png"
+  },
+  {
+    "revision": "0c378e158ae9ec27133f5469be833433",
+    "url": "/static/media/rdq.0c378e15.png"
+  },
+  {
+    "revision": "bc4e83cc25970d963d6a6d70c98e5595",
+    "url": "/static/media/hobbycool.bc4e83cc.png"
+  },
+  {
+    "revision": "b53504f9990fb45c1f53",
+    "url": "/static/css/2.14d97c79.chunk.css"
+  },
+  {
+    "revision": "bdf69c1ae2f5a9cf294766065168fd04",
+    "url": "/index.html"
+  }
+];
